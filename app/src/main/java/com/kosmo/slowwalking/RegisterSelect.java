d9a4908b1875bad08b711c74fcd8219db12b25b9
cysi230 <cysik94@gmail.com>
2021-02-06 15:58:27 +0900

    parents sitter

--- app/src/main/java/com/kosmo/slowwalking/RegisterSelect.java
+++ app/src/main/java/com/kosmo/slowwalking/RegisterSelect.java
@@ -1,7 +1,10 @@
 package com.kosmo.slowwalking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterSelect extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class RegisterSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_select);
+
+        ((Button)findViewById(R.id.ParentsRegister)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),
+                        ParentsRegister.class);
+                //액티비티 실행
+                startActivity(intent);
+            }
+        });
+
+
+        ((Button)findViewById(R.id.SitterRegister)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),
+                        SitterRegister.class);
+                //액티비티 실행
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/kosmo/slowwalking/SitterRegister.java
@@ -0,0 +1,13 @@
+package com.kosmo.slowwalking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SitterRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sitter_register);
+    }
+}
